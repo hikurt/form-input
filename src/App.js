229651dac456bac1ec0624f2556da454882db218
hikurt <32220244+hikurt@users.noreply.github.com>
2023-02-09 13:24:27 +0100

    used custom hook in BasicForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import SimpleInput from './components/SimpleInput';
+import BasicForm from './components/BasicForm';
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,24 +1,114 @@
 import React from 'react';
+import useInput from '../hooks/use-input';
+
+import '../index.css';
+
+const isNotEmpty = value => value.trim() !== '';
+const isEmail = value => value.includes('@');
 
 const BasicForm = (props) => {
+  const {
+    value:  firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameIsInvalid,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+
+  } = useInput(isNotEmpty);
+
+  const {
+    value:  lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameIsInvalid,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+
+  } = useInput(isNotEmpty);
+
+  const {
+    value:  emailValue,
+    isValid: emailIsValid,
+    hasError: emailIsInvalid,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+} = useInput(isEmail);
+
+  let formIsValid = false;
+
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    
+    if (!formIsValid) {
+      return;
+    }
+    console.log('Submitted data: ');
+    console.log(firstNameValue);
+    console.log(lastNameValue);
+    console.log(emailValue);
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  const firstNameClasses = firstNameIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
+    const lastNameClasses = lastNameIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
+    const emailClasses = emailIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={firstNameValue}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameIsInvalid && <p className='error-text'>Please enter a first name.</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameClasses}>
+          <label htmlFor='surname'>Last Name</label>
+          <input
+            type='text'
+            id='surname'
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+           {lastNameIsInvalid && <p className='error-text'>Please enter a last name.</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor='email'>E-Mail Address</label>
+        <input
+          type='email'
+          id='email'
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+         {emailIsInvalid && <p className='error-text'>Please enter a valid email.</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
